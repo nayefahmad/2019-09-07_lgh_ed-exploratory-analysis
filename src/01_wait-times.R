c80910b8c13c1281ffec10ac912ab30aaf50dfcb
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-09-08 00:40:11 -0700

    lognormal dist for nonadmit EDLOS

--- src/01_wait-times.R
+++ src/01_wait-times.R
@@ -17,7 +17,8 @@ library(tidyverse)
 library(lubridate)
 library(denodoExtractor)
 library(DT)
-
+library(broom)
+# library(fitdistrplus)
 
 setup_denodo()
 
@@ -92,7 +93,7 @@ df2.ed_data_modified %>%
                            buttons = c('excel', "csv")))
 
 
-
+#' ## Wait time distributions 
 #+ distributions
 # densities: ---------------
 df2.ed_data_modified %>% 
@@ -104,7 +105,22 @@ df2.ed_data_modified %>%
   ggplot(aes(x = ed_los)) +
   geom_density() + 
   facet_wrap(~start_year + is_admitted) + 
-  geom_vline(xintercept = 600, col = "blue")
+  geom_vline(xintercept = 600, col = "blue") + 
+  labs(title = "LGH ED: Start to disp times in minutes")
+
+# only nonadmit
+df2.ed_data_modified %>% 
+  select(ed_los, 
+         start_year, 
+         is_admitted) %>% 
+  filter(ed_los < quantile(ed_los, .99, na.rm = TRUE), 
+         is_admitted == "not_admitted") %>% 
+  
+  ggplot(aes(x = ed_los)) +
+  geom_density() + 
+  facet_wrap(~start_year + is_admitted) + 
+  geom_vline(xintercept = 600, col = "blue") + 
+  labs(title = "LGH ED: Start to disp times in minutes")
 
 
 #' Things to investigate:
@@ -114,3 +130,101 @@ df2.ed_data_modified %>%
 #'
 #' 2. Among admitted encntrs, there's clearly several different distributions
 #' smooshed together. Can we separate them by CTAS? Using variable `is_ed_admit_p4p_definition`? 
+#' 
+#' 
+#' 
+
+#' ### Nonadmits
+#' #### Are distributions lognormal and constant across years?  
+#' Reference: https://miningthedetails.com/blog/r/non-parametric-tests/
+v1.nonadmit_2017 <- 
+  df2.ed_data_modified %>% 
+  filter(start_year == "2017", 
+         is_admitted == "not_admitted") %>% 
+  select(ed_los) %>% 
+  na.omit() %>% 
+  pull(ed_los) # %>% density %>% plot
+
+v2.nonadmit_2018 <- 
+  df2.ed_data_modified %>% 
+  filter(start_year == "2018", 
+         ed_los < 5000, 
+         is_admitted == "not_admitted") %>%
+  select(ed_los) %>% 
+  na.omit() %>% 
+  pull(ed_los) # %>% density %>% plot
+
+v3.nonadmit_2019 <- 
+  df2.ed_data_modified %>% 
+  filter(start_year == "2019", 
+         ed_los < 50000, 
+         is_admitted == "not_admitted") %>%
+  select(ed_los) %>% 
+  na.omit() %>% 
+  pull(ed_los) # %>% density %>% plot
+
+
+stats::ks.test(v1.nonadmit_2017, 
+               v2.nonadmit_2018, 
+               alternative = "two.sided") %>% tidy()
+stats::ks.test(v2.nonadmit_2018, 
+               v3.nonadmit_2019, 
+               alternative = "two.sided") %>% tidy()
+
+
+fit_v1 <- (v1.nonadmit_2017 + 1) %>% fitdistrplus::fitdist("lnorm")
+plot(fit_v1)
+
+fit_v2 <- (v2.nonadmit_2018 + 1) %>% fitdistrplus::fitdist("lnorm")
+plot(fit_v2)
+
+fit_v3 <- (v3.nonadmit_2019+ 1) %>% fitdistrplus::fitdist("lnorm")
+plot(fit_v3)
+
+
+
+# summary(fit_v1)
+# summary(fit_v2)
+# summary(fit_v3)
+
+#' In practical terms, these are probably equivalent distributions. Reference
+#' above says that the D-stat will be between 0 and 1. When close to 1, very
+#' likely they're different distributions. When close to 0, the difference is
+#' very small.
+
+data.frame(x = v1.nonadmit_2017) %>% 
+  ggplot(aes(x = x)) + 
+  stat_ecdf(geom = "step") + 
+  geom_vline(xintercept = 600, col = "blue")
+
+
+#' Estimates for 2018 ED LOS distribution: 
+#' 
+data.frame(year = 2018, 
+           distribution = "lognormal", 
+           mean_log_scale = fit_v2$estimate[1] %>% unname, 
+           sd_log_scale = fit_v2$estimate[2] %>% unname, 
+           mean_minutes = exp(fit_v2$estimate[1] + (fit_v2$estimate[2]^2) * 1/2)) %>% 
+  gather() %>% 
+  datatable()
+
+
+
+
+# plot on log scale: 
+df2.ed_data_modified %>% 
+  select(ed_los, 
+         start_year, 
+         is_admitted) %>% 
+  filter(ed_los < quantile(ed_los, .99, na.rm = TRUE), 
+         is_admitted == "not_admitted") %>% 
+  
+  ggplot(aes(x = log(ed_los + 1))) +
+  geom_density() + 
+  facet_wrap(~start_year + is_admitted) + 
+  # geom_vline(xintercept = 600, col = "blue") + 
+  labs(title = "LGH ED: Start to disp times in minutes")
+
+
+#' ### Conclusion
+#'
